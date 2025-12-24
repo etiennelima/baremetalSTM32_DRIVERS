7e7f92ebde42ac71c4997f9d8c110c34859ebcf3
Etienne Felix de Lima <etienne.lima@inatel.br>
2025-12-24 10:15:30 -0300

    frist commit

---
+++ 0_test/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/core_cm4.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/cmsis_version.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/core_cm4.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/cmsis_version.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/CMSIS/Include/mpu_armv7.h:
+/home/etienne.lima/STM32CubeIDE/Advanced_BareMetal_Programming/0_test/chip_headers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
